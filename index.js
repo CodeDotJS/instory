32578680cc7ca629f8ebb59ec67d6b6887be7a33
CodeDotJS <rushforlinux@gmail.com>
2018-12-17 15:52:20 +0530

    simplify

--- index.js
+++ index.js
@@ -14,21 +14,11 @@ module.exports = (username, exclude) => {
 		const base = res.body.items;
 
 		const stories = {story: []};
-		const pack = [];
 
 		for (let i = 0; i < base.length; i++) {
-			base[i].video_versions === undefined ? stories.story.push(base[i].image_versions2.candidates[0].url) : stories.story.push(base[i].video_versions[0].url); // eslint-disable-line no-unused-expressions
+			base[i].video_versions === undefined ? stories.story.push(base[i].image_versions2.candidates[0].url) : stories.story.push(base[i].video_versions[0].url);
 		}
 
-		if (exclude) {
-			stories.story.map((x, i) => { // eslint-disable-line array-callback-return
-				pack[i] = x;
-			});
-
-			const feed = pack.filter(urls => urls.indexOf(obx[exclude]) === -1);
-
-			return feed;
-		}
-		return stories;
+		return exclude !== undefined ? stories.story.filter(urls => urls.indexOf(obx[exclude]) === -1) : stories;
 	});
 };
