7c23b5b1179215df92bed2e279bc8ced64892029
CodeDotJS <rushforlinux@gmail.com>
2018-12-17 10:52:33 +0530

    add tests

--- test.js
+++ test.js
@@ -5,3 +5,8 @@ test('getId', async t => {
 	const item = await m('9gag');
 	t.is(item.story[0], item.story[0]);
 });
+
+test('exclude', async t => {
+	const item = await m('9gag', 'image');
+	t.is(item[0], item[0]);
+});
